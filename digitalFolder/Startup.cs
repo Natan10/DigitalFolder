d90ea7036aab5c92c5df64937cb3667edcd3ef4a
Natan10 <natan-moreira1@hotmail.com>
2022-03-31 16:34:56 -0300

    Add entity framework and config connection string

--- digitalFolder/Startup.cs
+++ digitalFolder/Startup.cs
@@ -23,11 +23,12 @@ namespace digitalFolder
             services.AddControllers();
             
             /*
-                services.AddSwaggerGen(c =>
-                {
-                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "digitalFolder", Version = "v1" });
-                });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "digitalFolder", Version = "v1" });
+            });
             */
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
