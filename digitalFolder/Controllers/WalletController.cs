81ce1db83bcd18545179416041536a4895e9e984
Natan10 <natan-moreira1@hotmail.com>
2022-03-31 17:16:52 -0300

    Create Wallet and WalletController

---
+++ digitalFolder/Controllers/WalletController.cs
@@ -0,0 +1,26 @@
+﻿using DigitalFolder.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DigitalFolder.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalletController : ControllerBase
+    {
+        public WalletController()
+        {
+
+        }
+
+        // TODO - Get Wallet
+
+        [HttpPost]
+        public IActionResult CreateWallet(Wallet wallet)
+        {
+            Console.WriteLine($"Wallet: {wallet.WalletName} / {wallet.Balance} / {wallet.Description}");
+            return Ok(new {Name = wallet.WalletName , Balance = wallet.Balance , Description = wallet.Description});
+        }
+
+    }
+}
---
+++ digitalFolder/Models/Wallet.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DigitalFolder.Models
+{
+    public class Wallet
+    {
+        [Required]
+        [Key, Column(name: "id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "WalletName is required!")]
+        [StringLength(300, MinimumLength = 5)]
+        public string WalletName { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
